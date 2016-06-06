034526f2454aeedf482c25979a11b1f2e0e5adee
fatcloud <victor19871226@hotmail.com>
2016-06-06 20:00:11 +0800

    Add files via upload

---
+++ microsaver.sh
@@ -0,0 +1,25 @@
+cd ~/Desktop/beep
+ino build
+
+sleep 1
+echo 10
+sleep 1
+echo 9
+sleep 1
+echo 8
+sleep 1
+echo 7
+sleep 1
+echo 6
+sleep 1
+echo 5
+sleep 1
+echo 4
+sleep 1
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+
+ino upload
